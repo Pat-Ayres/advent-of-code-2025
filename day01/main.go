2c7b5dc01e495efd2d457a8e9e0e16525dd68090
Patrick Ayres <payres@panoramaed.com>
2025-12-02 09:40:14 -0500

    day one part one solution

--- day01/main.go
+++ day01/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
 
 func main() {
-	fmt.Println("Hello, Advent of Code!")
+	input = strings.TrimSuffix(input, "\n")
+	input = strings.ReplaceAll(input, "R", "")
+	input = strings.ReplaceAll(input, "L", "-")
+	dial := 50
+	count := 0
+	inputSlice := strings.SplitSeq(input, "\n")
+
+	for v := range inputSlice {
+		// turn the dial per the value
+		turn, err := strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+
+		// we only care if it lands on zero, with a wrapping range of 0..99
+		// so mod 100 on the turn keeps us within bounds
+		dial = (dial + turn) % 100
+
+		// check if we're on 0 and increment the count
+		if dial == 0 {
+			count++
+		}
+	}
+
+	fmt.Printf("part one solution: %d", count)
 }
