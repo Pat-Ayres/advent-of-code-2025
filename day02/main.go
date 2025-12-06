32240cd62b23604f16d2f786a3cf458f66ed2a8a
Patrick Ayres <payres@panoramaed.com>
2025-12-06 14:02:10 -0500

    day 2 part 1 solution

---
+++ day02/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+var inputSlice []string
+
+func init() {
+	input = strings.TrimSuffix(input, "\n")
+	inputSlice = strings.Split(input, ",")
+}
+
+func main() {
+	partOne()
+}
+
+func partOne() {
+	total := 0
+
+	for _, v := range inputSlice {
+		firstLastSlice := strings.Split(v, "-")
+		firstValue, _ := strconv.Atoi(firstLastSlice[0])
+		lastValue, _ := strconv.Atoi(firstLastSlice[1])
+		for i := firstValue; i <= lastValue; i++ {
+			strVal := strconv.Itoa(i)
+			strLen := len(strVal)
+			if strLen%2 != 0 {
+				continue
+			}
+
+			mid := strLen / 2
+			if strVal[:mid] == strVal[mid:] {
+				total = total + i
+			}
+		}
+	}
+
+	fmt.Printf("Total: %d", total)
+}
