7427001bca83b819f778d4a2fdef41c12f612b2b
Patrick Ayres <payres@panoramaed.com>
2025-12-07 22:53:43 -0500

    day 2 full solution

--- day02/main.go
+++ day02/main.go
@@ -19,6 +19,7 @@ func init() {
 
 func main() {
 	partOne()
+	partTwo()
 }
 
 func partOne() {
@@ -29,19 +30,67 @@ func partOne() {
 		firstValue, _ := strconv.Atoi(firstLastSlice[0])
 		lastValue, _ := strconv.Atoi(firstLastSlice[1])
 		for i := firstValue; i <= lastValue; i++ {
-			strVal := strconv.Itoa(i)
-			strLen := len(strVal)
-			if strLen%2 != 0 {
-				continue
-			}
-
-			mid := strLen / 2
-			power := int(math.Pow10(mid)) + 1
-			if i%power == 0 {
+			if t := hasSequence(i, 2); t {
 				total = total + i
 			}
 		}
 	}
 
 	fmt.Printf("Part One Total: %d\n", total)
+}
+
+func partTwo() {
+	total := 0
+
+	for _, v := range inputSlice {
+		firstLastSlice := strings.Split(v, "-")
+		firstValue, _ := strconv.Atoi(firstLastSlice[0])
+		lastValue, _ := strconv.Atoi(firstLastSlice[1])
+		for i := firstValue; i <= lastValue; i++ {
+			switch {
+			case hasSequence(i, 2):
+				total = total + i
+			case hasSequence(i, 3):
+				total = total + i
+			case hasSequence(i, 5):
+				total = total + i
+			case hasSequence(i, 7):
+				total = total + i
+			}
+		}
+	}
+
+	fmt.Printf("Part Two Total: %d\n", total)
+}
+
+func hasSequence(s int, numSequences int) bool {
+	strVal := strconv.Itoa(s)
+	strLen := len(strVal)
+
+	// if the value's number of digits is not evenly divisible
+	// by the number of sequences, it cannot have many sequences
+	if strLen%numSequences != 0 {
+		return false
+	}
+
+	seqSize := strLen / numSequences
+
+	// for a sequence k to repeat multiple times in an integer, you
+	// left shift it and fill it in with itself. For each repetition,
+	// it is left shifted by its own length. This can be reversed by
+	// dividing the resulting value by 10 left shifted the same number
+	// of times, plus 1 for filling itself into the zeroes. This is
+	// expressed mathematically for sequence k occurring n times as:
+	// divisor = 10^(k*(n-1))+10^(k*(n-2))+...+10^k+1
+	var divisor int
+	for i := numSequences - 1; i > 0; i-- {
+		divisor = divisor + int(math.Pow10(seqSize*i))
+	}
+
+	divisor = divisor + 1
+
+	if s%divisor == 0 {
+		return true
+	}
+	return false
 }
